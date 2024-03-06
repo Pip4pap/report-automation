9e1920b734ef0f1fba7c447a1d566090e3a82f6e
pip4pap <phillip.musumba.52@gmail.com>
2024-03-07 01:11:42 +0300

    Replaces boiler plate react page with custom landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import UploadSheet from './pages/UploadSheet';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="font-bold text-5xl mb-3">LAMPS report automation</h1>
+        <Router>
+          <Routes>
+            <Route path="/" element={<UploadSheet/>} />
+          </Routes>
+        </Router>
       </header>
     </div>
   );
