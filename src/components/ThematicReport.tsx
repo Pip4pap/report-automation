2d19135e77722400ea38cfe05596aa7bbf827a1a
Pip4pap <phillip.musumba.52@gmail.com>
2024-11-26 14:16:58 +0300

    Darkens LIN value's font color

--- src/components/ThematicReport.tsx
+++ src/components/ThematicReport.tsx
@@ -340,7 +340,7 @@ const ThematicReport: React.FC<ThematicReportProps> = ({ thematicReportData }) =
                   borderBottomWidth: '2.667px',
                   borderBottomColor: '#AC4888'
                 }}
-                className="-mt-1 text-center text-green-700 font-normal overflow-hidden"
+                className="-mt-1 text-center text-green-900 font-normal overflow-hidden"
               >{ thematicReportData.LIN }</div>
             </div>
           </div>
